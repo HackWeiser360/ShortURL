441c2703ca848391dcc254687701b1b2616184d4
MådMâx <74961526+HackWeiser360@users.noreply.github.com>
2020-12-28 11:49:43 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ShortURL
-Shorten your URL/link with this tool!!
+ShortURL is a shell based script for shortening your URL/link. It is available in Termux(both rooted&non rooted devices) and Parrot OS. Get more tools from HackWeiser360 [here](https://GitHub.com/HackWeiser360) 
+
+***
+### [+] Installation and Usage
+* $ apt update
+* $ apt install git -y
+* $ git clone https://github.com/HackWeiser360/ShortURL
+* $ cd ShortURL
+* $ bash setup.sh
+
+### Type `shorturl` or `ShortUrl` to run the Tool.
+### Or, Use Single Command if you are lazy😂
+
+* $ apt update && apt install git -y && git clone https://github.com/HackWeiser360/ShortURL && cd shorturl && bash setup.sh && shorturl
+***
+
+### Follow on [Instagram](https://www.instagram.com/madmax4708/)
+### Visit our YouTube channel. [Why not?](https://youtube.com/channel/UC02OkpTZkxRZCEzFjawf6mA)
+### Visit our [FaceBook Page](fb.me/HackWeiser360)
+### You can leave a message [here](m.me/HackWeiser360)
---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+banner() {
+clear
+printf "\e[0m\n"
+printf "\e[0m\e[1;91m    _____ _                _     _    _ _____  _      \e[0m\n"
+printf "\e[0m\e[1;91m   / ____| |              | |   | |  | |  __ \| |     \e[0m\n"
+printf "\e[0m\e[1;91m  | (___ | |__   ___  _ __| |_  | |  | | |__) | |     \e[0m\n"
+printf "\e[0m\e[1;91m   \___ \|  _ \ / _ \|  __| __| | |  | |  _  /| |     \e[0m\n"
+printf "\e[0m\e[1;91m   ____) | | | | (_) | |  | |_  | |__| | | \ \| |____ \e[0m\n"
+printf "\e[0m\e[1;91m  |_____/|_| |_|\___/|_|  \___|  \____/|_|  \_\______|\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;92m        Created By HackWeiser360 \e[0m\e[1;91m( \e[0m\e[1;92mMådMâx \e[0m\e[1;91m)\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m    Twitter :\e[0m\e[1;96m 503_madmax \e[0m\e[1;97m| \e[0m\e[1;93mInstagram :\e[0m\e[1;96m madmax4708\e[0m\n"
+}
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installing Packages..\e[0m\n"
+printf "\e[0m\n"
+apt update
+apt install curl -y
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Creating Environment...\e[0m\n"
+printf "\e[0m\n"
+cp -f url /data/data/com.termux/files/usr/bin/shorturl
+cp -f url /data/data/com.termux/files/usr/bin/ShortUrl
+echo 'termux-open-url https://github.com/HackWeiser360/' > /data/data/com.termux/files/usr/bin/HackWeiser360
+echo 'termux-open-url https://github.com/HackWeiser360/' > /data/data/com.termux/files/usr/bin/HackWeiser360
+echo 'termux-open-url https://www.instagram.com/madmax4708/' > /data/data/com.termux/files/usr/bin/instagram
+echo 'termux-open-url https://www.instagram.com/madmax4708/' > /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/HackWeiser360
+chmod +x /data/data/com.termux/files/usr/bin/HackWeiser360
+chmod +x /data/data/com.termux/files/usr/bin/instagram
+chmod +x /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/shorturl
+chmod +x /data/data/com.termux/files/usr/bin/ShortUrl
+sleep 2
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installation Succeed !!\e[0m\n"
+sleep 2
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Type \e[0m\e[1;92mShortUrl \e[0m\e[1;93mto Run this Tool \e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\n"
